19ae2b7706d98bd193393b0826ca40057b08572a
emangalloway <emanuelgalloway25@gmail.com>
2025-05-12 10:46:14 -0400

    Finished Dealership file Manager get dealership method

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private  ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+
+    }
+
+    public void getAllVehicle(){
+
+    }
+
+
+
+}
---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,47 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class DealershipFileManager {
+
+    public void getDealership(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("dealership.csv"));
+            String firstLine = bufferedReader.readLine();
+            String[] firstLineParts = firstLine.split("\\|");
+                String name = firstLineParts[0];
+                String address = firstLineParts[1];
+                String phone = firstLineParts[2];
+
+                Dealership dealership = new Dealership(name,address,phone);
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null){
+                    String[] parts = line.split("\\|");
+                    int vin = Integer.parseInt(parts[0]);
+                    int year = Integer.parseInt(parts[1]);
+                    String make = parts[2];
+                    String model = parts[3];
+                    String vehicleType = parts[4];
+                    String color = parts[5];
+                    int odometer = Integer.parseInt(parts[6]);
+                    Double price = Double.parseDouble(parts[7]);
+
+                    Vehicle vehicle = new Vehicle(vin,year,make,model,vehicleType,color,odometer,price);
+                }
+
+
+        }catch (Exception e){
+            System.out.println("An error has occured");
+        }
+
+    }
+
+    public  void saveDealership(Dealership){
+
+    }
+
+}
