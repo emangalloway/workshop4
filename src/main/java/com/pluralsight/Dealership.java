700b16922306124cbe3684e284e2f8bc4dca0d46
emangalloway <emanuelgalloway25@gmail.com>
2025-05-12 16:33:32 -0400

    Working on methods inside of Dealership class so i can return the value and use it in the UI class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -27,10 +27,38 @@ public class Dealership {
     public String getName() {
         return name;
     }
-    public ArrayList<Vehicle> getVehicleNyPrice(double min, double max){}
-    public ArrayList<Vehicle>  getVehicleByMakeModel(String make, String model){}
-    public ArrayList<Vehicle>  getVehicleByYear(double min,double max){}
-    public ArrayList<Vehicle> getVehicleByColor(String color){}
+    public ArrayList<Vehicle> getVehicleByPrice(double min, double max){
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getPrice() >= min && vehicle.getPrice() <= max){
+                results.add(vehicle);
+            }
+        }return results;
+    }
+    public ArrayList<Vehicle>  getVehicleByMakeModel(String make, String model){
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)){
+                results.add(vehicle);
+            }
+        }return results;
+    }
+    public ArrayList<Vehicle>  getVehicleByYear(double min,double max){
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getYear() >= min && vehicle.getYear() <= max){
+                results.add(vehicle);
+            }
+        }return results;
+    }
+    public ArrayList<Vehicle> getVehicleByColor(String color){
+        ArrayList<Vehicle> results = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getColor().equalsIgnoreCase(color)){
+                results.add(vehicle);
+            }
+        }return results;
+    }
     public ArrayList<Vehicle> getVehicleByMileage(double min,double max){}
     public ArrayList<Vehicle> getVehicleByType(String vehicleType){}
     public ArrayList<Vehicle> getAllVehicle(){}
