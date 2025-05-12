6d6b2e425119f53d0f46813dc6b8a2686e644b79
emangalloway <emanuelgalloway25@gmail.com>
2025-05-12 14:24:25 -0400

    Working on the menu

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -16,8 +16,48 @@ public class UserInterface {
         boolean running = true;
 
         while (running){
-            System.out.println("Welcome to"+dealership.getName());
-            System.out.println();
+            System.out.println("Welcome to "+dealership.getName());
+            System.out.println("Choose an option");
+            System.out.println("1.) Find Vehicle within a price range");
+            System.out.println("2.) Find Vehicle by make/model");
+            System.out.println("3.) Find Vehicle by year");
+            System.out.println("4.) Find Vehicle by color");
+            System.out.println("5.) Find Vehicle by mileage");
+            System.out.println("6.) Find Vehicle by type");
+            System.out.println("7.) List All Vehicles");
+            System.out.println("8.) Add a vehicle");
+            System.out.println("9.) Remove a vehicle");
+            System.out.println("99.) Quit");
+
+            Integer input = scanner.nextInt();
+
+            switch (input){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 99:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option please try again.");
+                    break;
+
+            }
 
         }
     }
